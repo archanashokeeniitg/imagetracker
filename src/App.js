e85ab6673940d5f338a298b70de87c5d37421044
archanashokeeniitg <archanashokeeniitg@gmail.com>
2022-05-13 15:55:31 -0700

    phase 0

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 //amplify exports
-import { Amplify, Auth } from "aws-amplify";
-import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify, { Auth, Hub } from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
 import awsExports from "./aws-exports";
+import { AmazonAIPredictionsProvider } from "@aws-amplify/predictions";
 
+//components imports
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 
 Amplify.configure(awsExports);
-Auth.configure(awsExports);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 function App() {
   const [user, setUser] = useState("");
   useEffect(() => {
     document.body.style.background =
       "linear-gradient(45deg, #51547e 30%, #0a3f74 90%)";
+
+    //finding and setting loggedin User
+    let updateUser = async (authState) => {
+      try {
+        let user = await Auth.currentAuthenticatedUser();
+        setUser(user);
+      } catch {
+        setUser(null);
+      }
+    };
+    Hub.listen("auth", updateUser); // listen for login/signup events
+    updateUser(); // check manually the first time because we won't get a Hub event
+    return () => Hub.remove("auth", updateUser); // cleanup
   }, []);
+
   return (
     <AmplifyAuthenticator style={{ textAlign: "center" }}>
       <div className="App">
-        <row>
-          <Header />
-        </row>
-        Hello I'm clean!!
+        {user ? <Header user={user} /> : null}
         <Footer />
       </div>
     </AmplifyAuthenticator>
@@ -33,55 +46,90 @@ function App() {
 
 export default App;
 
-// import React, { useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 // import "./App.css";
 
 // //amplify exports
-// import Amplify from "aws-amplify";
+// import { Amplify, Auth } from "aws-amplify";
 // import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 // import awsExports from "./aws-exports";
 
+// import Header from "./components/layout/Header";
+// import Footer from "./components/layout/Footer";
+
 // Amplify.configure(awsExports);
+// Auth.configure(awsExports);
 
 // function App() {
+//   const [user, setUser] = useState("");
 //   useEffect(() => {
 //     document.body.style.background =
 //       "linear-gradient(45deg, #51547e 30%, #0a3f74 90%)";
 //   }, []);
 //   return (
 //     <AmplifyAuthenticator style={{ textAlign: "center" }}>
-//       <div className="App">Hello I'm clean!!</div>
+//       <div className="App">
+//         <row>
+//           <Header />
+//         </row>
+//         Hello I'm clean!!
+//         <Footer />
+//       </div>
 //     </AmplifyAuthenticator>
 //   );
 // }
 
 // export default App;
 
-// // import logo from "./logo.svg";
+// // import React, { useEffect } from "react";
 // // import "./App.css";
+
+// // //amplify exports
 // // import Amplify from "aws-amplify";
+// // import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 // // import awsExports from "./aws-exports";
+
 // // Amplify.configure(awsExports);
 
 // // function App() {
+// //   useEffect(() => {
+// //     document.body.style.background =
+// //       "linear-gradient(45deg, #51547e 30%, #0a3f74 90%)";
+// //   }, []);
 // //   return (
-// //     <div className="App">
-// //       <header className="App-header">
-// //         <img src={logo} className="App-logo" alt="logo" />
-// //         <p>
-// //           Edit <code>src/App.js</code> and save to reload.
-// //         </p>
-// //         <a
-// //           className="App-link"
-// //           href="https://reactjs.org"
-// //           target="_blank"
-// //           rel="noopener noreferrer"
-// //         >
-// //           Learn React
-// //         </a>
-// //       </header>
-// //     </div>
+// //     <AmplifyAuthenticator style={{ textAlign: "center" }}>
+// //       <div className="App">Hello I'm clean!!</div>
+// //     </AmplifyAuthenticator>
 // //   );
 // // }
 
 // // export default App;
+
+// // // import logo from "./logo.svg";
+// // // import "./App.css";
+// // // import Amplify from "aws-amplify";
+// // // import awsExports from "./aws-exports";
+// // // Amplify.configure(awsExports);
+
+// // // function App() {
+// // //   return (
+// // //     <div className="App">
+// // //       <header className="App-header">
+// // //         <img src={logo} className="App-logo" alt="logo" />
+// // //         <p>
+// // //           Edit <code>src/App.js</code> and save to reload.
+// // //         </p>
+// // //         <a
+// // //           className="App-link"
+// // //           href="https://reactjs.org"
+// // //           target="_blank"
+// // //           rel="noopener noreferrer"
+// // //         >
+// // //           Learn React
+// // //         </a>
+// // //       </header>
+// // //     </div>
+// // //   );
+// // // }
+
+// // // export default App;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,26 +1,17 @@
-// import React from "react";
-
-// function Header() {
-//   return <div>Header</div>;
-// }
-
-// export default Header;
-
 import React from "react";
 
 //components
 import Home from "../Home";
 import Upload from "../Upload";
 
-import { Router, Route, useLocation } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import { createBrowserHistory as createHistory } from "history";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 const history = createHistory();
 
-const Header = () => {
-  //   const location = useLocation();
+const Header = (props) => {
   return (
     <div>
       <Router history={history}>
@@ -40,15 +31,73 @@ const Header = () => {
               </NavLink>
             </NavItem>
           </Nav>
+          <NavItem className="text-white " style={{ paddingLeft: "40%" }}>
+            <i className="fa fa-user " aria-hidden="true"></i>
+            &nbsp;
+            {props.user.username} &nbsp;
+          </NavItem>
           <AmplifySignOut />
         </Navbar>
-        <Route path="/about">
-          <Home />
-        </Route>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} /> */}
+        <Route path="/" exact component={Home} />
+        <Route path="/upload" exact component={Upload} />
       </Router>
     </div>
   );
 };
+
 export default Header;
+
+// // import React from "react";
+
+// // function Header() {
+// //   return <div>Header</div>;
+// // }
+
+// // export default Header;
+
+// import React from "react";
+
+// //components
+// import Home from "../Home";
+// import Upload from "../Upload";
+
+// import { Router, Route, useLocation } from "react-router-dom";
+// import { createBrowserHistory as createHistory } from "history";
+// import { AmplifySignOut } from "@aws-amplify/ui-react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+// const history = createHistory();
+
+// const Header = () => {
+//   //   const location = useLocation();
+//   return (
+//     <div>
+//       <Router history={history}>
+//         <Navbar color="dark">
+//           <NavbarBrand href="/" className="text-white">
+//             Image Gallery
+//           </NavbarBrand>
+//           <Nav>
+//             <NavItem>
+//               <NavLink className="text-white" href="/">
+//                 Home
+//               </NavLink>
+//             </NavItem>
+//             <NavItem>
+//               <NavLink className="text-white" href="/upload">
+//                 upload Image
+//               </NavLink>
+//             </NavItem>
+//           </Nav>
+//           <AmplifySignOut />
+//         </Navbar>
+//         <Route path="/about">
+//           <Home />
+//         </Route>
+//         {/* <Route path="/" element={<Home />} />
+//         <Route path="/upload" element={<Upload />} /> */}
+//       </Router>
+//     </div>
+//   );
+// };
+// export default Header;
