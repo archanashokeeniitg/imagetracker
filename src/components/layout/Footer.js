01174940af4d54549b7016f238940b5461e0232f
archanashokeeniitg <archanashokeeniitg@gmail.com>
2022-05-13 16:48:56 -0700

    phase stable

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function footer() {
   return (
     <div className="fixed-bottom bg-dark text-white">
-      image Gallery@2020
+      image Gallery@2022
       <br />
-      created by Trinity
+      created by Archana
     </div>
   );
 }
