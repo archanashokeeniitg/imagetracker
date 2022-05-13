ccd0bce2d217fe9c472338efb307644a5e508993
archanashokeeniitg <archanashokeeniitg@gmail.com>
2022-05-13 16:13:03 -0700

    phase 1

---
+++ src/components/ImageGallery.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+// import "./ImageGallery.css";
+import {
+  Card,
+  Button,
+  CardTitle,
+  CardImg,
+  CardText,
+  Row,
+  Col,
+} from "reactstrap";
+
+class ImageGallery extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log("inside Imagegallery", this.props.images);
+    return (
+      <div className="container">
+        <Row>
+          {this.props.images.map((image) => (
+            <Col>
+              <Card key={image.id} className="  jumbotron ">
+                <div>
+                  <i
+                    className="fa fa-trash pull-left "
+                    aria-hidden="true"
+                    onClick={(event) => {
+                      this.props.deleteImage(image.id);
+                    }}
+                  ></i>
+                  <i
+                    className="fa fa-download   pull-right "
+                    onClick={(event) => {
+                      this.props.downloadImage(image.src);
+                    }}
+                  ></i>
+                  <CardImg
+                    className=""
+                    src={image.src}
+                    alt="Smiley face"
+                    width="100"
+                    height="200"
+                  />
+                </div>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ImageGallery;
